7f70f10bd9519f2e83da52fb70a7e81da6620eed
Ryoya Izu <rizu@mba.local>
2026-04-21 20:56:27 +0900

    Initial commit

---
+++ ALDS1/ALDS1_1_D.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Ryoya Izu on 2026/04/21.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int R[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> R[i];
+    }
+    int diff = -10000000000;
+    int diff_tmp;
+
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j=i+1; j < n; j++)
+        {
+            diff_tmp = R[j] - R[i];
+            if (diff_tmp > diff)
+            {
+                diff = diff_tmp;
+            }
+        }
+    }
+    cout << diff << endl;
+    return 0;
+}
